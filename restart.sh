2e5e0976b16856efea54e3457c84db7a7defdedb
Doug Anson <doug.anson@arm.com>
2018-08-26 15:06:20 -0500

    updates

--- restart.sh
+++ restart.sh
@@ -2,7 +2,8 @@
 
 cd ${HOME}/service
 ./killService.sh
-cp ${HOME}/service/logs/pelion-bridge.log ${HOME}/pelion-bridge-$(date +%F-%H:%M:%S).log
+mkdir -p ${HOME}/log_archive 2>&1 1>/dev/null
+cp ${HOME}/service/logs/pelion-bridge.log ${HOME}/log_archive/pelion-bridge-$(date +%F-%H:%M:%S).log
 sleep 2
 ./runService.sh &
 cd ${HOME}
